a3eea6d11798feb400d05bdddd312cadec45d069
eva montgomery <eva.m.montgomery@gmail.com>
2020-01-14 12:42:09 -0500

    adds routing solutions

---
+++ index.js
@@ -0,0 +1,128 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const bodyParser = require('body-parser');
+const parseForm = bodyParser.urlencoded({
+    extended: true
+});
+
+const { dateToFormattedString } = require('./utils');
+
+
+// Use this if you're building an API
+// that accepts JSON data from the client.
+// const parseJson = bodyParser.json();
+
+// This is what you'll see in tutorials.
+// I think it's not so great.
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+const server = http.createServer(app);
+
+const pets = require('./models/pets');
+
+
+// See all pets!
+app.get('/pets', async (req, res) => {
+    const thePets = await pets.all();
+    // res.send('you want /pets');
+    res.json(thePets);
+
+    // Here's what `res.json()` is doing:
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json'
+    // })
+    // const jsonString = JSON.stringify(thePets);
+    // res.write(jsonString);
+    // res.end();
+});
+
+app.get('/pets/:id(\\d+)', async (req, res) => {
+    console.log('you want to get by id');
+    // show me a single pet by their id
+    const { id } = req.params;
+    const thePet = await pets.one(id);
+    res.json(thePet);
+});
+
+// Hint: npm i body-parser
+
+// Create
+app.get('/pets/create', (req, res) => {
+    console.log('you want the form');
+    // console.log('yes you are at /pets/create');
+    // res.send('yes you are at /pets/create');
+
+    // express will look in templates/pets/form.html
+    res.render('pets/form', {
+        locals: {
+            name: '',
+            species: '',
+            birthdate: ''
+        }
+    });
+});
+app.post('/pets/create', parseForm, async (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.species);
+    console.log(req.body.birthdate);
+
+    const { name, species, birthdate } = req.body;
+    // const name = req.body.name;
+    // const species = req.body.species;
+    // const birthdate = req.body.birthdate;
+
+    // I could create a new pet!
+    // and I'm going to hard code the owner id!
+
+    const owner_id_CHANGEME = 1;
+    const newPetId = await pets.create(name, species, birthdate, owner_id_CHANGEME);
+    console.log(`The new pet id is ${newPetId}`);
+
+    res.redirect(`/pets/${newPetId}`);
+});
+
+// Update
+app.get('/pets/:id/edit', async (req, res) => {
+
+    const { id } = req.params;
+    // const id = req.params.id;
+
+    const thePet = await pets.one(id);
+
+    res.render('pets/form', {
+        locals: {
+            name: thePet.name,
+            species: thePet.species,
+            birthdate: dateToFormattedString(thePet.birthdate)
+        }
+    });
+});
+app.post('/pets/:id/edit', parseForm, async (req, res) => {
+    const { name, species, birthdate } = req.body;
+    const { id } = req.params;
+    const result = await pets.update(id, name, species, birthdate);
+    if (result) {
+        res.redirect(`/pets/${id}`);
+    } else {
+        res.redirect(`/pets/${id}/edit`)
+    }
+});
+
+// Delete
+app.get('/pets/:id/delete')
+app.post('/pets/:id/delete')
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
---
+++ models/pets.js
@@ -0,0 +1,197 @@
+const db = require('./connection');
+const { dateToFormattedString } = require('../utils');
+
+// Create
+async function create(name, species, birthdate, owner_id) {
+    const result = await db.one(`
+insert into pets
+    (name, species, birthdate, owner_id)
+values
+    ($1, $2, $3, $4)    
+returning id
+    `, [name, species, birthdate, owner_id]);
+
+    return result.id;
+}
+
+// Retrieve
+async function one(id) {
+    try {
+        // Use .one() if there should exactly one result.
+        // const onePet = await db.one(`select * from pets where id=${id}`);
+
+        // $1 is syntax specfic to pg-promise
+        // it means interpolate the 1st value from the array
+        // (in this case, that's the `id` that we received as an argument)
+        const onePet = await db.one(`select * from pets where id=$1`, [id]);
+        return onePet;
+    } catch (err) {
+        return null;
+    }
+}
+
+// Promise version
+// function one(id) {
+//     return db.one(`select * from pets where id=${id}`)
+//                 .catch(err => {
+//                     console.log(err);
+//                     return null;
+//                 })
+// }
+
+async function all() {
+    try {
+        // .query and .any are the same function
+        // const thePets = await db.query(`select * from pets;`);
+        const thePets = await db.any(`select * from pets;`);
+        console.log(thePets);
+        return thePets;
+    } catch (err) {
+        console.log(err)
+        return [];
+    }
+}
+
+// Promise version
+// function all() {
+//     return db.query(`select * from pets;`)
+//             .catch(err => {
+//                 return []
+//             });
+// }
+
+
+// async function all() {
+//     const allPets = await db.query(`select * from pets`)
+//         .then(data => {
+//             console.log(data);
+//             return data;
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             return [];
+//         });
+    
+//     console.log(allPets);
+//     return allPets;
+// }
+
+// Update
+// function updateName(id, name) {
+//     const resultPromise = db.result(`
+//         update pets set
+//             name=$1
+//         where id=$2;
+//     `, [name, id]);
+//     resultPromise.then(result => {
+//         if (result.rowCount === 1) {
+//             return id;
+//         } else {
+//             return null;
+//         }
+//     });
+//     return resultPromise;
+// }
+
+async function updateName(id, name) {
+    const result = await db.result(`
+        update pets set
+            name=$1
+        where id=$2;
+    `, [name, id]);  
+    // return result;
+
+    // if we needed to do another
+    // db call, we would need to
+    // await it separately.
+    // const anotherResult = await db.any();
+
+    if (result.rowCount === 1) {
+        return id;
+    } else {
+        return null;
+    }
+}
+
+async function updateSpecies(id, species) {
+    const result = await db.result(`
+        update pets set
+            species=$1
+        where id=$2;
+    `, [species, id]);  
+    // return result;
+
+    // if we needed to do another
+    // db call, we would need to
+    // await it separately.
+    // const anotherResult = await db.any();
+
+    if (result.rowCount === 1) {
+        return id;
+    } else {
+        return null;
+    }
+}
+
+
+
+async function updateBirthdate(id, dateObject) {
+    // Postgres wants this: '2020-01-13'
+    const dateString = dateToFormattedString(dateObject);
+    const result = await db.result(`
+        update pets set
+            birthdate=$1
+        where id=$2
+    `, [dateString, id]);
+    return result;
+}
+
+
+async function update(id, name, species, birthdate) {
+    const result = await db.result(`
+        update pets set
+            name=$2,
+            species=$3,
+            birthdate=$4
+        where id=$1;
+    `, [id, name, species, birthdate]);  
+    // return result;
+
+    // if we needed to do another
+    // db call, we would need to
+    // await it separately.
+    // const anotherResult = await db.any();
+
+    if (result.rowCount === 1) {
+        return id;
+    } else {
+        return null;
+    }
+}
+
+
+// Delete
+async function del(id) {
+    const result = await db.result(`delete from pets where id=$1`, [id]);
+    console.log(result);
+    if (result.rowCount === 1) {
+        return id;
+    } else {
+        return null;
+    }
+}
+
+
+module.exports = {
+    create,
+    one,
+    all,
+    update,
+    updateName,
+    updateBirthdate,
+    updateSpecies,
+    del
+}
+
+
+
